c1960cc051c77bd29a96594d7da0b5bc17d948d5
VasuBhakt <cpswastik31@gmail.com>
2026-07-10 08:21:13 +0000

    added epoll support

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,8 @@
 #include <map>
 #include <netdb.h>
 #include <netinet/tcp.h>
-#include <poll.h>
+// #include <poll.h>
+#include <sys/epoll.h>
 #include <string>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -78,21 +79,30 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  // watchlist for sockets
-  std::vector<struct pollfd> poll_fds;
+  // Create epoll instance
+  int epoll_fd = epoll_create1(0);
+  if (epoll_fd == -1) {
+    std::cerr << "Failed to create epoll instance\n";
+    return 1;
+  }
 
-  // 1. Add the server_fd to the watchlist first
-  struct pollfd server_pfd = {0}; // Initialize all fields (including revents) to zero
-  server_pfd.fd = server_fd;
-  server_pfd.events = POLLIN; 
-  poll_fds.push_back(server_pfd);
+  // Add server to epoll instance using epoll_ctl
+  struct epoll_event ev;
+  ev.events = EPOLLIN;
+  ev.data.fd = server_fd;
+  if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev) == -1) {
+    std::cerr << "Failed to add server_fd to epoll\n";
+    return 1;
+  }
+
+  const int MAX_EVENTS = 1024;
+  struct epoll_event events[MAX_EVENTS];
 
   while (true) {
     // 2. Ask the OS to wait until SOMETHING happens
-    // poll (list(pointer to vector), size, timeout_in_ms)
-    int new_events = poll(poll_fds.data(), poll_fds.size(), 100);
+    int new_events = epoll_wait(epoll_fd, events, MAX_EVENTS, 100);
     if (new_events < 0) {
-      std::cerr << "Poll failed\n";
+      std::cerr << "epoll_wait failed\n";
       break;
     }
     if (new_events == 0) {
@@ -100,57 +110,54 @@ int main(int argc, char **argv) {
       continue;
     }
     // 3. Loop through our watchlist to see who signaled us
-    //size_t current_size = poll_fds.size();
-    for (size_t i = 0; i < poll_fds.size(); i++) {
-      if (poll_fds[i].revents & POLLIN) { // does this fd have something new?
-        if (poll_fds[i].fd == server_fd) {
-          // EVENT :  New connection
-          struct sockaddr_in client_addr = {0};
-          socklen_t client_addr_len = sizeof(client_addr);
-          int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
-                                 &client_addr_len);
-          if (client_fd >= 0) {
-            std::cout << "Client connected!\n";
-            // Disable Nagle's algorithm to prevent delayed ACK
-            // interaction that kills pipelining throughput
-            int flag = 1;
-            setsockopt(client_fd, IPPROTO_TCP, TCP_NODELAY, &flag,
-                       sizeof(flag));
-            // add NEW client to watchlist
-            struct pollfd client_pfd = {0};
-            client_pfd.fd = client_fd;
-            client_pfd.events = POLLIN;
-            poll_fds.push_back(client_pfd);
-            clients_map[client_fd] = Client{};
+    for (int i = 0; i < new_events; i++) {
+      int current_fd = events[i].data.fd;
+      if (current_fd == server_fd) {
+        // EVENT :  New connection
+        struct sockaddr_in client_addr = {0};
+        socklen_t client_addr_len = sizeof(client_addr);
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+                               &client_addr_len);
+        if (client_fd >= 0) {
+          std::cout << "Client connected!\n";
+          // Disable Nagle's algorithm to prevent delayed ACK
+          // interaction that kills pipelining throughput
+          int flag = 1;
+          setsockopt(client_fd, IPPROTO_TCP, TCP_NODELAY, &flag,
+                     sizeof(flag));
+          // add NEW client to watchlist
+          struct epoll_event client_ev;
+          client_ev.events = EPOLLIN;
+          client_ev.data.fd = client_fd;
+          if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &client_ev) == -1) {
+            std::cerr << "Failed to add client_fd to epoll\n";
+            close(client_fd);
+            continue;
           }
+          clients_map[client_fd] = Client{};
+        }
+      } else {
+        // EVENT : An existing client sent a message (PING)
+        char buffer[16384];
+        int bytes_recieved =
+            recv(current_fd, buffer, sizeof(buffer), 0);
+        if (bytes_recieved <= 0) {
+          std::cout << "Client disconnected\n";
+          epoll_ctl(epoll_fd, EPOLL_CTL_DEL, current_fd, nullptr);
+          pubsub_cleanup_client(current_fd);
+          clients_map.erase(current_fd);
+          close(current_fd);
         } else {
-          // EVENT : An existing client sent a message (PING)
-          int current_client_fd = poll_fds[i].fd;
-          char buffer[16384];
-          int bytes_recieved =
-              recv(current_client_fd, buffer, sizeof(buffer), 0);
-          if (bytes_recieved <= 0) {
-            std::cout << "Client disconnected\n";
-            pubsub_cleanup_client(current_client_fd);
-            clients_map.erase(current_client_fd);
-            close(current_client_fd);
-            // poll_fds.erase(poll_fds.begin()+i); O(n) in worst time
-            // this is ABSOLUTE GENIUS!
-            poll_fds[i] = poll_fds.back();
-            poll_fds.pop_back();
-            //current_size--;
-            i--;
-          } else {
-            clients_map[current_client_fd].parser.parse_and_execute(
-                buffer, bytes_recieved, current_client_fd);
-            // send(current_client_fd, response, strlen(response), 0);
-            // flush_aof();
-          }
+          clients_map[current_fd].parser.parse_and_execute(
+              buffer, bytes_recieved, current_fd);
+          // send(current_fd, response, strlen(response), 0);
+          // flush_aof();
         }
       }
     }
     flush_aof(); // Periodic flush (everysec check inside)
   }
   close(server_fd);
+  close(epoll_fd);
   return 0;
 }
